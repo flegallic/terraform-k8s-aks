a6d96db9fe8a9303c0e1bd4b937cde4ce54beb52
fabrice.legallic <fabrice.legallic@orange.com>
2019-08-19 16:36:53 +0200

    add config files

---
+++ k8s-aks/variables.tf
@@ -0,0 +1,17 @@
+variable "subscription_id" {description = "The Microsoft Azure Provider"}
+variable "client_id" {description = "The Microsoft Azure Provider"}
+variable "client_secret" {description = "The Microsoft Azure Provider"}
+variable "tenant_id" {description = "The Microsoft Azure Provider"}
+variable "agent_count" {}
+variable "ssh_public_key" {}
+variable "dns_prefix" {}
+variable cluster_name {}
+variable resource_group_name {}
+variable location {}
+variable log_analytics_workspace_name {}
+variable log_analytics_workspace_location {}
+variable log_analytics_workspace_sku {}
+variable vm_size {}
+variable os_type {}
+variable os_disk_size_gb {}
+variable admin_username {}
