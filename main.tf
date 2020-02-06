42501bf0fe43d0965e4094fd1026f8405132b17c
haxware <fabrice.legallic@gmail.com>
2020-02-06 22:27:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,16 +6,16 @@ module "k8s-aks" {
     client_secret                       = "${var.client_secret}"
     tenant_id                           = "${var.tenant_id}"
     location                            = "eastus"
-    agent_count                         = 3
+    agent_count                         = 5
     dns_prefix                          = "k8saks"
     cluster_name                        = "k8saks"
     resource_group_name                 = "k8saks-ResourceGroup"
     log_analytics_workspace_name        = "k8saks-LogAnalyticsWorkspaceName"
     log_analytics_workspace_location    = "eastus"
     log_analytics_workspace_sku         = "PerGB2018"
-    vm_size                             = "Standard_B1S"
+    vm_size                             = "Standard_B2s"
     os_type                             = "Linux"
-    os_disk_size_gb                     = "9"
+    os_disk_size_gb                     = "30"
     admin_username                      = "ubuntu"
     ssh_public_key                      = "~/.ssh/id_rsa.pub"
 }
